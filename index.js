d6f222af8af11037e26fd98ea242af3457c9ad2e
prashant kumar <thearticle35@gmail.com>
2021-10-27 11:51:57 +0530

    updted the page

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ var date = new Date();
 var year = date.getFullYear();
 
 document.getElementById('date').textContent = year;
+
+// var phone = document.querySelector('#contact').value;
+
+// var regex = /\d{5}([- ]*)\d{6}/;
+
+// if (!regex.test(phone)) {
+//     document.getElementById('#contact-span').style.display = 'block';
+// }
+
+function table() {
+    document.getElementById('medicinetable-container').style.width = '100%';
+}
+
+
+function closebtn() {
+    document.getElementById('medicinetable-container').style.width = '0%';
+}
