fd4cf538419918de5517abf9806ad251d396b3b2
prashant kumar <thearticle35@gmail.com>
2021-10-28 09:07:29 +0530

    updated the page
    added tables for every category

--- index.js
+++ index.js
@@ -21,11 +21,69 @@ document.getElementById('date').textContent = year;
 //     document.getElementById('#contact-span').style.display = 'block';
 // }
 
-function table() {
-    document.getElementById('medicinetable-container').style.width = '100%';
+function table1() {
+    document.getElementById('medicinetable-container1').style.width = '100%';
 }
 
 
-function closebtn() {
-    document.getElementById('medicinetable-container').style.width = '0%';
+function closebtn1() {
+    document.getElementById('medicinetable-container1').style.width = '0%';
+}
+
+
+
+
+function table2() {
+    document.getElementById('medicinetable-container2').style.width = '100%';
+}
+
+
+function closebtn2() {
+    document.getElementById('medicinetable-container2').style.width = '0%';
+}
+
+
+
+
+function table3() {
+    document.getElementById('medicinetable-container3').style.width = '100%';
+}
+
+
+function closebtn3() {
+    document.getElementById('medicinetable-container3').style.width = '0%';
+}
+
+
+
+function table4() {
+    document.getElementById('medicinetable-container4').style.width = '100%';
+}
+
+
+function closebtn4() {
+    document.getElementById('medicinetable-container4').style.width = '0%';
+}
+
+
+
+function table5() {
+    document.getElementById('medicinetable-container5').style.width = '100%';
+}
+
+
+function closebtn5() {
+    document.getElementById('medicinetable-container5').style.width = '0%';
+}
+
+
+
+
+function table6() {
+    document.getElementById('medicinetable-container6').style.width = '100%';
+}
+
+
+function closebtn6() {
+    document.getElementById('medicinetable-container6').style.width = '0%';
 }
